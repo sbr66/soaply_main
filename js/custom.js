86712b836d079ac9f09606cbf677955b76377e5b
sbr66 <bonam22@gmail.com>
2022-12-07 12:50:54 +0900

    sign up ui complete

--- js/custom.js
+++ js/custom.js
@@ -1,29 +1,3 @@
-/********** Elements Clone For Mobile*********/
-const mobileMenus = document.querySelector(".mobile-menus");
-const navs = document.querySelector(".nav-lists").cloneNode(true);
-const info = document.querySelector(".info").cloneNode(true);
-
-mobileMenus.appendChild(navs);
-mobileMenus.appendChild(info);
-
-/********** Mobile Menu Toggle *********/
-const mobileBtn = document.querySelector(".mobile-btn");
-
-toggleMobileBtn = (e) => {
-  const target = e.currentTarget;
-  const menuHeight = mobileMenus.scrollHeight; //scrollheight : 지정 대상의 높이값을 읽어준다.
-  target.classList.toggle("active");
-
-  if (target.classList.contains("active")) {
-    target.classList.remove("not-active");
-    mobileMenus.style.height = menuHeight + "px"; //`${menuHeight}px`
-  } else {
-    target.classList.add("not-active");
-    mobileMenus.style.height = 0;
-  }
-};
-mobileBtn.addEventListener("click", toggleMobileBtn);
-
 /********** Header Change Effect*********/
 const header = document.querySelector("#header");
 const stickyHeader = () => {
