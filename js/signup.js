5e9a803e68cbde2812093b9c78f78fd82f37154b
sbr66 <bonam22@gmail.com>
2022-12-15 12:48:22 +0900

    sign up complete

--- js/signup.js
+++ js/signup.js
@@ -4,6 +4,8 @@ const idInput = document.querySelector(".id");
 const emailInput = document.querySelector(".email");
 const pwdInput = document.querySelector(".pwd");
 const rpwdInput = document.querySelector(".rpwd");
+// http response code 참조 : https://www.whatap.io/ko/blog/40/
+let status;
 
 signUp.addEventListener("click", () => {
   // 입력창 작성 체크
@@ -43,8 +45,21 @@ signUp.addEventListener("click", () => {
   // 입력창 작성 체크 끝 : 위 부분이 완료 되면 다음 코드로 진행
   //   formdata 참조 : https://ko.javascript.info/formdata
   const formData = new FormData(document.querySelector("form"));
-  fetch()("/main_backend/model.php", {
+  fetch("/main_backend/model/register.php", {
     method: "POST",
     body: formData,
-  });
+  })
+    .then((res) => {
+      console.log(res);
+      status = res.status;
+      return res.json();
+    })
+    .then((resData) => {
+      // console.log(resData);
+      alert(resData.msg);
+      location.href = "/main_project/index.html";
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
