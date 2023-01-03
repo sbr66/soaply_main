ac7ee25156cfbd5ebda744d09eefaeecc98c1341
sbr66 <bonam22@gmail.com>
2023-01-03 12:23:06 +0900

    comment update data

--- js/requestCmt.js
+++ js/requestCmt.js
@@ -4,6 +4,7 @@ const url = document.location.href;
 const urlIndex = Number(url.split("=")[1]);
 console.log(`p_idx: ${urlIndex}`);
 
+// 상품평 작성
 cmtBtn.addEventListener("click", async () => {
   // 입력창 작성 체크 (아무것도 입력 안하고 입력 버튼 누를시 경고창)
   if (!cmtInputBox.value) {
@@ -55,41 +56,122 @@ const getCmtList = async () => {
       // console.log(lists);
       listCount.textContent = lists.length;
       let listsElmt;
-      lists.map((list) => {
-        if (list.user_id === list.session_id) {
-          listsElmt = `
-            <form onsubmit="return false">
-              <div class="comment-list">
-                <div class="list-info">
-                  <p>${list.user_id} |</p>
-                  <em>${list.cmt_reg} |</em>
-                  <button type="submit" class="cmt-update">수정하기</button>
-                </div>
-                <div class="list-content">
-                  <p>
-                    ${list.cmt_cont}
-                  </p>
-                </div>
-              </div>
-            </form>
-            `;
-        } else {
+      lists.map((list, idx) => {
+        if (list.user_id === "guest") {
           listsElmt = `<div class="comment-list">
             <div class="list-info">
               <p>${list.user_id}</p>
               <em>${list.cmt_reg}</em>
             </div>
-            <div class="list-content">
+            <div class="list-content" id="list-${idx}">
               <p>
                 ${list.cmt_cont}
               </p>
             </div>
           </div>`;
+        } else {
+          if (list.user_id === list.session_id) {
+            listsElmt = `           
+              <div class="comment-list">
+                <div class="list-info">
+                  <p>${list.user_id} |</p>
+                  <em>${list.cmt_reg} |</em>
+                  <button type="button" class="cmt-update">수정하기</button>
+                </div>
+                <div class="list-content" id="list-${idx}">
+                  <p>
+                    ${list.cmt_cont}
+                  </p>
+                </div>
+              </div>
+            `;
+          } else {
+            listsElmt = `<div class="comment-list">
+            <div class="list-info">
+              <p>${list.user_id}</p>
+              <em>${list.cmt_reg}</em>
+            </div>
+            <div class="list-content" id="list-${idx}">
+              <p>
+                ${list.cmt_cont}
+              </p>
+            </div>
+          </div>`;
+          }
         }
         cmtWrapper.innerHTML += listsElmt;
       });
+      // 수정하기 기능 분리 선언
+      updateCmt(lists);
     })
     .catch((err) => console.log(err));
 };
 
 getCmtList();
+
+// 수정하기 기능 함수 선언
+function updateCmt(cmtObjs) {
+  console.log(cmtObjs);
+  const cmtUpBtns = document.querySelectorAll("button.cmt-update");
+  // console.log(cmtUpBtns);
+  if (cmtObjs.length !== 0 && cmtUpBtns) {
+    cmtUpBtns.forEach((btn) => {
+      //console.log(btn);
+      btn.addEventListener("click", function () {
+        // console.log(this);
+        // 노드 추적은 공백(엔터)을 포함한다.
+        const changeInput = this.parentNode.nextSibling.nextSibling;
+        const thisIdx = changeInput.getAttribute("id").split("-")[1];
+        //console.log(thisIdx);
+        // console.log(changeInput);
+
+        this.classList.toggle("active");
+        if (btn.classList.contains("active")) {
+          this.textContent = "취소하기";
+          changeInput.innerHTML = `
+            <form onsubmit="return false;" class="update-form update-form-${thisIdx}">
+              <input type="text" name="update_cont" value="${cmtObjs[thisIdx].cmt_cont}">
+              <button type="submit">수정입력</button>
+            </form>
+          `;
+
+          const udSubmitBtn = document.querySelector(
+            `.update-form-${thisIdx} button`
+          );
+
+          udSubmitBtn.addEventListener("click", async function () {
+            const formData = new FormData(
+              document.querySelector(`.update-form-${thisIdx}`)
+            );
+            await fetch(
+              `/main_backend/model/cmt_ctrl.php?cmt_idx=${cmtObjs[thisIdx].cmt_idx}&req_sign=patch_cmt`,
+              {
+                method: "PATCH",
+                body: formData,
+              }
+            )
+              .then((res) => {
+                //console.log(res);
+                //status = res.status;
+                return res.json();
+              })
+              .then((resData) => {
+                alert(resData.msg);
+                //console.log(resData);
+                location.reload();
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          });
+        } else {
+          this.textContent = "수정하기";
+          changeInput.innerHTML = `
+            <p>${cmtObjs[thisIdx].cmt_cont}</p>
+          `;
+        }
+      });
+    });
+  } else {
+  }
+}
